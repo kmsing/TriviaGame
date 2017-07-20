a1838ae7178ce6e789094427d22717a4e9dd44a5
Kristine Singleton <kmsing11@gmail.com>
2017-07-20 08:09:41 -0500

    adding files into repo

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+
+
+var actualchoices=new Array()
+document.cookie="ready=yes"
+var totalquestions=5
+
+var timeleft = 20;
+var downloadTimer = setInterval(function(){
+	timeleft--;
+	document.getElementById("countdowntimer").textContent = timeleft;
+	if(timeleft <= 0)
+	clearInterval(downloadTimer);
+	},1000);
+
+//solutions to each question
+var correctchoices=new Array()
+correctchoices[1]='c' 
+correctchoices[2]='b' 
+correctchoices[3]='c'
+correctchoices[4]='d'
+correctchoices[5]='d'
+
+function gradeit(){
+var incorrect=null
+for (q=1;q<=totalquestions;q++){
+	var thequestion=eval("document.myquiz.question"+q)
+	for (c=0;c<thequestion.length;c++){
+		if (thequestion[c].checked==true)
+		actualchoices[q]=thequestion[c].value
+		}
+		
+	if (actualchoices[q]!=correctchoices[q]){ //incorrect choice
+		if (incorrect==null)
+		incorrect=q
+		else
+		incorrect+="/"+q
+		}
+	}
+
+
